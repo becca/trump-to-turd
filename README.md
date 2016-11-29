3a48503572b5e95227198d77f173f9050bba444f
Becca Z <rzandstein@gmail.com>
2016-11-29 17:12:43 -0600

    power to the bowl

---
+++ README.md
@@ -0,0 +1,10 @@
+trump-to-turd
+=============
+
+![](logo.png)
+
+Chrome extension that replaces occurrences of 'Trump' with 'Turd'
+
+INSTALLATION
+
+In Chrome, navigate to chrome://extensions and select the trump-to-turd source files under Load Unpacked Extensions.
---
+++ Source/content_script.js
@@ -0,0 +1,43 @@
+walk(document.body);
+
+function walk(node)
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+
+	var child, next;
+
+	// if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
+	//    || node.classList.indexOf('ace_editor') > -1) {
+  //	return;
+  //	}
+
+	switch ( node.nodeType )
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child )
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode)
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bTrump\b/g, "Turd");
+	v = v.replace(/\btrump\b/g, "Turd");
+
+	textNode.nodeValue = v;
+}
